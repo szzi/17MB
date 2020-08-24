41ae869fece214e42d1d372c5751e5085d921679
yallyyally <jhshin0028@gmail.com>
2020-08-24 22:03:40 +0900

    Client socket(working on,,..)
    type parsing 필요

--- styler/socketModule/websocket.js
+++ styler/socketModule/websocket.js
@@ -1,6 +1,7 @@
 // need to change to websocket server's address
 var wsUri = "ws://192.168.43.6:9999";
 var output;
+var flag;
 
 function init()
 {
@@ -22,25 +23,72 @@ function testWebSocket()
 
 function onOpen(evt)
 {
-  writeToScreen("CONNECTED");
+  // writeToScreen("CONNECTED");
+  //flag : 최초인지아닌지 분간용
+  flag=0;
   console.log("Connected");
   doSend();
+ 
 }
 
 function onClose(evt)
 {
-  writeToScreen("DISCONNECTED");
+  console.log("DISCONNECTED");
 }
 
 function onMessage(evt)
 {
-  // var message = JSON.parse(evt.data);
+  /*
+  if (flag)
+  {
+    //cnt값으로 판별 -> 숫자가 적으면 카메라 열고, 숫자가 크면 걍 무시...
+    //type1->cnt 값으로 판별, type2->문열기
+    
+    if(cnt<5)
+    {
+        console.log("camera open!");
+        location.href="../../"
+    }
+    else
+    {
+
+    }
+  }
+  else //flag=0 -> initial connection msg
+  {
+    setTimeout(function() {doSend();}, 5000);
+    flag=1;
+  }*/
+  //var message = JSON.parse(evt.data);
   var message = evt.data;
   writeToScreen('<span style="color: blue;">RESPONSE: ' + message +'</span>');
   // Call Notification method here
   console.log(message)
-  print(message);
-  websocket.close();
+  /*
+  console.log(message.type);
+  console.log(message.state);
+*/
+  // websocket.close();
+
+  /*message parsing */
+/*
+  if (message.type=="1")
+  {
+    console.log("type1");
+  }
+  else if (message.type=="2")
+  {
+    console.log("type2");
+  }
+  else if(message.type=="3")
+  {
+    console.log("type3");
+  }
+  else{
+    console.log("what?!");
+  }
+*/
+setTimeout(function() {doSend();}, 5000);
 }
 
 function onError(evt)
