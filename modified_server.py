c809b750c1abd061525f53233955e33c9160053b
JaeYeong <42104898+JaeYeong-Li@users.noreply.github.com>
2020-09-03 17:34:34 +0900

    Update modified_server.py

--- modified_server.py
+++ modified_server.py
@@ -8,15 +8,21 @@ from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 import json
 #humidity
 import math
-curHum=0
-successcnt = 0
+#convert
+from ast import literal_eval
+
+
 class SimpleEcho(WebSocket):
+
+
+    def handleConnected(self):
+        print(self.address, 'open')
+
     def handleMessage(self):
-        requestString = json.dumps(self.data)
-        print(requestString)
-        type_=0
-        # responseString = '{"state":"connected", "type":"0", "message":""}'
-        #connect sensor
+        is_open = 0 #1일때 열려있음
+        is_work = 0 #습도,스타일링 둘중 하나라도 일하고 있음 / 스타일링 중/ 습도 조절중
+        
+        #connect sensord
         led_R=20
         led_Y=21
         PIR_sensor=17
@@ -28,12 +34,13 @@ class SimpleEcho(WebSocket):
         GPIO.setup(PIR_sensor, GPIO.IN)
         GPIO.setup(servomotor_pin, GPIO.OUT)
         print("PIR Ready..")
-        # time.sleep(2)
+        time.sleep(2)
         #PMW: change pulse
         p= GPIO.PWM(servomotor_pin, 50)
         sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
+        
         if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
             sensor = sensor_args[sys.argv[1]]
             pin = sys.argv[2]
@@ -42,95 +49,106 @@ class SimpleEcho(WebSocket):
             print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
             print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
             sys.exit(1)
+        
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-        flag=0
         p.start(0)
         p.ChangeDutyCycle(2.5) #0
         print("servo, humidity Ready..")
-        is_break = 0
-        type_=0
-        successcnt_dt=0
-        successcnt_ndt=0
-        try:
-            while True:
-                ##PIR sensor
-                #DETECTED
-                if GPIO.input(PIR_sensor) == 1:
-                    GPIO.output(led_Y, 1)
-                    GPIO.output(led_R, 0)
-                    # is_break=1
-                    # print("type 1",successcnt)
-                    type_=1
-                    successcnt_dt = successcnt_dt+1
-                    print("detected!",successcnt_dt)
-                    time.sleep(1)
-                    # responseString = '{"state":"connected", "type":'+''str(type_)+ "message":"'+str(successcnt)+'"}'
-                    if successcnt_dt>20:
-                        successcnt_dt=0
-                        break
-                    else:
-                        continue
-                #NOT DETECTED
-                else:
-                    successcnt_dt=0
-                    GPIO.output(led_R, 1)
-                    GPIO.output(led_Y, 0)
-                    print("not detected !")
-                    #survo motor
-                    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-                    if humidity is not None and temperature is not None:
-                        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-                        humidity = math.floor(humidity)
-                        print(humidity)
-                        if humidity>70:
-                        # if flag==0:
-                            type_=2
-                            # flag=1
-                            p.ChangeDutyCycle(7.5) #90
-                            time.sleep(1)
-                            p.ChangeDutyCycle(5.0)
-                            while humidity>70:
-                                humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-                                time.sleep(1)
-                            p.ChangeDutyCycle(2.5) #0
-                            # time.sleep(1)
-                            print("///////////////////STOP HERE/////////////")
-                            time.sleep(5)
-                            # print("is_break:",is_break,"flag:",flag)
-                            # if is_break!=1:
-                            #     is_break=1
-                                # print("type 2")
-                            # type_=2
+        time.sleep(2)
+        
+        requestObject = json.dumps(self.data)
+        # requestString = '{'work':'ticket' || '1'}'
+        # responseString = '{"work":"0(cam) || 2(dehumid)", "user":"name"}'
+
+        requestString = literal_eval(requestObject)
+        obj = json.loads(requestString)
+        print(obj["work"])
+        msg = obj["work"]
+
+
+
+        #클라이언트 메세지에서 (ticket 혹은 1) is_work[1] on/off 결정(스타일링 여부) 
+        if msg == "1":
+            print("start sytling")
+            is_work=1 #작동중임,,
+            time.sleep(10)
+            print("styling done")
+            is_work=0
+            responseString = '{"work":"", "username":""}'
+
+        else : 
+            try:
+                while True:
+                    ##PIR sensor
+                    #Human detected && not working
+                    if GPIO.input(PIR_sensor) == 1 and is_work==0:
+                        GPIO.output(led_Y, 1)
+                        GPIO.output(led_R, 0)
+                        # type_=1
+                        print("detected, count start..")
+                        time.sleep(5)
+                        print("count done")
+
+                        if GPIO.input(PIR_sensor) == 1:
+                            is_work=1
+                            # 카메라 켜고 사람 인식 start
+                            # send user name
+                            responseString = '{"work":"0", "username":"name"}'
                             break
-                                # responseString = '{"state":"connected", "type":"2", "message":""}'
                         else:
-                        # if flag == 1:
-                            p.ChangeDutyCycle(7.5) #0
-                            # flag=0
-                    # time.sleep(1)
+                            #적외선 인식작업 재개
+                            continue
+                            
+                    #Human not detected || working (door is opening , styling ...)
                     else:
-                        print('Failed to get reading. Try again!')
-                        sys.exit(1)
-                # if is_break==1:
-                #     is_break=0
-                #     break
-                # if successcnt < 1000:
-                #     successcnt += 1
-        except KeyboardInterrupt:
-            print("Stopped by User")
-            p.stop()
-            GPIO.cleanup()
-            type_=-1
-            # responseString = '{"state":"disconnected", "type":"-1", "message":""}'
-            responseString = '{"state":"connected", "type":'+str(type_)+ '"message":'+str(successcnt)+'}'
-            self.sendMessage(responseString)
-        print("I will send msg & initialize variable~")
-        successcnt_dt=0
-        responseString = '{"state":"connected", "type":'+str(type_)+ '"message":'+str(successcnt)+'}'
+                        GPIO.output(led_R, 1)
+                        GPIO.output(led_Y, 0)
+                        print("not detected !")
+                        #survo motor
+                        if is_work == 0 or is_open == 1:
+                            print('door is opened or machine not working')
+                            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+                            if humidity is not None and temperature is not None:
+                                print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+                                humidity = math.floor(humidity)
+                                print(humidity)
+                                if humidity>70:
+                                    #door is closed but humidity is high-> we should open
+                                    if is_open==0:
+                                        #open
+                                        p.ChangeDutyCycle(7.5) #90
+                                        is_open=1
+                                        time.sleep(1)
+                                        is_work = 1
+                                        responseString = '{"work":"2", "username":"open"}'
+                                        break
+                                #we should close
+                                else:
+                                    if is_open==1:
+                                        p.ChangeDutyCycle(5.0)
+                                        is_open = 0
+                                        is_work = 0
+                                        responseString = '{"work":"2", "username":"close"}'
+                                        break
+                        else:
+                            print("it is  working and it is closed")
+
+            except KeyboardInterrupt:
+                print("Stopped by User")
+                p.stop()
+                GPIO.cleanup()
+                # type_=-1
+                # responseString = '{"state":"disconnected", "type":"-1", "message":""}'
+                # responseString = '{"state":"connected", "type":'+str(type_)+ '"message":'+str(successcnt)+'}'
+                self.sendMessage(responseString)
+        
+
+        #메시지 내용: 인식한 사람 이름
+        print("send message")
+        # successcnt_dt=0
         print(responseString)
         self.sendMessage(responseString)
-    def handleConnected(self):
-        print(self.address, 'open')
+   
     def handleClose(self):
         print(self.address, 'closed')
 server = SimpleWebSocketServer('',9999, SimpleEcho)
