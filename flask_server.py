92e8aa424008e90f6db428cc8b76abef74c3bcd4
yallyyally <jhshin0028@gmail.com>
2020-09-11 14:24:40 +0900

    이미지파일 정리
    .png 파일 static 폴더로 이동

--- flask_server.py
+++
@@ -1,9 +0,0 @@
-#web server on webos (python Flask)
-from flask import Flask,render_template
-
-app = Flask(__name___
-
-#default page
-@app.route("/")
-def index():
-    return render_template('../index.html')
